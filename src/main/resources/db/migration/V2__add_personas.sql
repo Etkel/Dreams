464aa08cf246a830ca1691a70c9a49eff604fc0d
Etkel <etkeldelacroix@gmail.com>
2023-05-23 16:54:36 +0300

    small fixes

--- src/main/resources/db/migration/V2__add_personas.sql
+++ src/main/resources/db/migration/V2__add_personas.sql
@@ -1,11 +1,11 @@
 insert into personas (email, last_name, name, password, role)
-values ('admin@gmail.com', 'admin', 'admin', '1234', 0);
+values ('admin@gmail.com', 'admin', 'admin', '$2a$12$cAINsHmVway0LYk4atd5D.TG.zEIK1oCBPvJAAjuZCjA/n6/PKjAS', 0);
 insert into carts (persona_id) values (1);
 
 insert into personas (email, last_name, name, password, role)
-values ('user@gmail.com', 'user', 'user', '1234', 1);
+values ('user@gmail.com', 'user', 'user', '$2a$12$iPyF.X6vnQIr2D4bvNjzy.M2Y6c2WyXSd/OvV6dE7hHC1VXaQYAA2', 1);
 insert into carts (persona_id) values (2);
 
 insert into personas (email, last_name, name, password, role)
-values ('ollan@gmail.com', 'Jonsan', 'Ollan', '1234', 1);
+values ('ollan@gmail.com', 'Jonsan', 'Ollan', '$2a$12$oFRjTOr8ZZU91DuD.LbSPuGDSi1tBkIhvCNByP63TUlexvtqN3fuG', 1);
 insert into carts (persona_id) values (3);
